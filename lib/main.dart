865a97139c9085245b5eee4d458336782ba96f64
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2020-11-08 12:01:29 +0100

    this is just for testing

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      darkTheme: ThemeData.dark(),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }

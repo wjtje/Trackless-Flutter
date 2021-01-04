2a07897fdf52f3a74b62800bec822f06f67ab067
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2021-01-04 14:24:53 +0100

    🔥 Removed debug code

--- lib/pages/account/account.dart
+++ lib/pages/account/account.dart
@@ -1,44 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:trackless/global/app_state.dart';
 import 'package:trackless/components/async_progress.dart';
 import 'package:trackless/pages/account/account_information.dart';
 import 'package:trackless/pages/account/account_options.dart';
 import 'package:trackless/pages/account/account_refresh_action.dart';
-import 'package:trackless/trackless/trackless_account.dart';
-import 'package:trackless/trackless/trackless_failure.dart';
 
 final accountPage = AppPage(
     pageTitle: 'account_page_title',
     page: AccountPage(),
-    appBarActions: [
-      AccountRefreshAction(),
-      Builder(
-          builder: (context) => IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                final accountState =
-                    Provider.of<TracklessAccount>(context, listen: false);
-
-                accountState.tracklessUser = null;
-              })),
-      Builder(
-          builder: (context) => IconButton(
-                icon: Icon(Icons.folder_open),
-                onPressed: () async {
-                  final accountState =
-                      Provider.of<TracklessAccount>(context, listen: false);
-
-                  try {
-                    await accountState.refreshFromLocalStorage();
-                  } on TracklessFailure catch (e) {
-                    // Something went wrong
-                    // Create a snackbar to alert the user
-                    e.displayFailure();
-                  }
-                },
-              ))
-    ]);
+    appBarActions: [AccountRefreshAction()]);
 
 class AccountPage extends StatelessWidget {
   const AccountPage({Key key}) : super(key: key);
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -4,9 +4,6 @@ import 'package:trackless/global/app_state.dart';
 import 'package:trackless/components/async_progress.dart';
 import 'package:trackless/components/list_work/list_work.dart';
 import 'package:trackless/pages/home/home_refresh_action.dart';
-import 'package:trackless/pages/work_dialog/work_dialog.dart';
-import 'package:trackless/trackless/trackless_failure.dart';
-import 'package:trackless/trackless/trackless_location.dart';
 import 'package:trackless/trackless/trackless_work.dart';
 
 import 'home_floating_action_button.dart';
@@ -15,43 +12,7 @@ final homePage = new AppPage(
     pageTitle: 'this_week_page_title',
     page: HomePage(),
     floatingActionButton: HomeFloatingActionButton(),
-    appBarActions: [
-      HomeRefreshAction(),
-      Builder(
-          builder: (context) => IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                final tracklessWorkProvider =
-                    Provider.of<TracklessWorkProvider>(context, listen: false);
-                final tracklessLocationProvider =
-                    Provider.of<TracklessLocationProvider>(context,
-                        listen: false);
-
-                tracklessWorkProvider.workList = null;
-                tracklessLocationProvider.locationList = null;
-              })),
-      Builder(
-          builder: (context) => IconButton(
-              icon: Icon(Icons.folder_open),
-              onPressed: () async {
-                final tracklessWorkProvider =
-                    Provider.of<TracklessWorkProvider>(context, listen: false);
-                final tracklessLocationProvider =
-                    Provider.of<TracklessLocationProvider>(context,
-                        listen: false);
-
-                try {
-                  await tracklessWorkProvider.refreshFromLocalStorage(
-                      tracklessWorkProvider.startDate,
-                      tracklessWorkProvider.endDate);
-                  await tracklessLocationProvider.refreshFromLocalStorage();
-                } on TracklessFailure catch (e) {
-                  // Something went wrong
-                  // Create a snackbar to alert the user
-                  e.displayFailure();
-                }
-              }))
-    ]);
+    appBarActions: [HomeRefreshAction()]);
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
